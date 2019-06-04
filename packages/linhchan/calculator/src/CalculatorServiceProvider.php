226f325a2af89d14ed6144e839716155876a57cd
linh <linhvtm@tech.est-rouge.com>
2019-06-04 11:07:04 +0700

    initial commit

---
+++ packages/linhchan/calculator/src/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Linhchan\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('Linhchan\Calculator\CalculatorController');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
